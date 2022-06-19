43d1b51fe98a986fe9fdfa463a97e3f31ccc83fd
Kamil Jarosz <kamil@MS-7D09-b5582992.localdomain>
2022-06-19 14:01:21 +0000

    Simple drawing code

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,7 +4,26 @@ module Lib
 
 import           Data.Text
 import           Data.GI.Base
+import           GHC.Int
 import qualified GI.Gtk                        as Gtk
+import           GI.Cairo.Render.Connector (renderWithContext, toRender)
+import qualified GI.Cairo.Render as Cairo
+
+contentWidth :: Int32
+contentWidth = 800
+
+contentHeight :: Int32
+contentHeight = 600
+
+fillBackground :: Cairo.Render Bool
+fillBackground = do
+  Cairo.save
+  Cairo.setSourceRGB 0 0 0
+  Cairo.rectangle 0 0 300 200
+  Cairo.fill
+  Cairo.stroke
+  Cairo.restore
+  return True
 
 showWindow :: IO ()
 showWindow = do
@@ -12,11 +31,15 @@ showWindow = do
 
   win <- Gtk.windowNew Gtk.WindowTypeToplevel
   Gtk.windowSetTitle win (pack "Introduction")
-  Gtk.windowResize win 640 480
+  Gtk.windowResize win contentWidth contentHeight
   Gtk.onWidgetDestroy win Gtk.mainQuit
 
-  message <- Gtk.labelNew (Just (pack "Hello"))
-  Gtk.containerAdd win message
+  drawingArea <- Gtk.drawingAreaNew
+  Gtk.widgetSetHexpand drawingArea True
+  Gtk.widgetSetVexpand drawingArea True
+  Gtk.containerAdd win drawingArea
+
+  Gtk.onWidgetDraw drawingArea (renderWithContext fillBackground)
 
   Gtk.widgetShowAll win
   Gtk.main
